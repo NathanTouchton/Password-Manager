dfb4b16aa6df506038eba1be2d931e8f21daf52b
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-08-14 20:42:26 -0500

    Add files via upload

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,12 +1,42 @@
-from tkinter import Tk, Canvas, PhotoImage, Button, Label, Entry
+from tkinter import Tk, Canvas, PhotoImage, Button, Label, Entry, messagebox
+from random import randint, shuffle, choice
+from pyperclip import copy
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def add_func():
-    with open(file="password-list.txt", mode="a") as file:
-        file.write(f"{website_entry.get()} | {u_name_entry.get()} | {password_entry.get()}\n")
+    website = website_entry.get()
+    u_name = u_name_entry.get()
+    password = password_entry.get()
+    if website == "" or u_name == "" or password == "":
+        messagebox.showerror(title="Oops", message="Please don't leave any fields empty.")
+
+    else:
+        is_ok = messagebox.askokcancel(title=website, message=f"Details entered: {u_name}\nPassword: {password}\nSave?")
+
+        if is_ok:
+            with open(file="password-list.txt", mode="a") as file:
+                file.write(f"{website} | {u_name} | {password}\n")
+            website_entry.delete(0, "end")
+            password_entry.delete(0, "end")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -40,7 +70,7 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
-gen_pass = Button(text="Generate Password", width=15)
+gen_pass = Button(text="Generate Password", width=15, command=generate_password)
 gen_pass.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=37, command=add_func)
